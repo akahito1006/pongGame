1fdc991881c9a47259c34b1f063ccf755a68d996
akahito1006 <nekodora5k@gmail.com>
2022-04-23 20:31:04 +0900

    fragmentTransaction from activity to Fragment

--- app/src/main/java/com/example/learntoplay/MainActivity.kt
+++ app/src/main/java/com/example/learntoplay/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.learntoplay
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.learntoplay.databinding.ActivityMainBinding
+import com.example.learntoplay.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    // set up the instance of binding on activity
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // viewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // fragmentTransaction
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment()) // removed companion object
+                .commit() // commitNow() has no addToBackStack() internally
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/learntoplay/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.example.learntoplay.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.learntoplay.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    // set up the instance of binding on fragment
+    private var _binding: FragmentMainBinding? = null
+    // onCreateView - onDestroyView
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // viewBinding
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
